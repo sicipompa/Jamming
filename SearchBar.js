c7b0c05fbd55a0d283f6dff4c300f09f7cd12783
sicipompa <110480198+sicipompa@users.noreply.github.com>
2022-10-17 14:31:06 -0500

    Add files via upload

---
+++ SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    handleTermChange(e){
+        this.setState({
+            searchValue: e.target.value
+        })
+    }
+
+    search(){
+        this.props.onSearch(this.state.searchValue)
+    }
+    render(){
+        return(
+        <div className="SearchBar">
+            <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"/>
+            <button className="SearchButton">SEARCH</button>
+        </div>)
+    }
+}
+
+export default SearchBar;
